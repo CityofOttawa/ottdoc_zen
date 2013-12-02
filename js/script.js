892a57fba2e8544d699d139ad7729e16b3a4cc29
Tyler Pepper <pepperzz@gmail.com>
2013-12-02 15:35:08 -0500

    Merging updated code.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+jQuery(document).ready(function($) {
+  // Add placeholder functionality for <= ie9
+  $('input, textarea').placeholder();
+});
