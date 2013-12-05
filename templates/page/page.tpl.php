775b510cc663f61dd0116f0f6cb2b47c949572c1
Tiffany Tse <ttse@coldfrontlabs.ca>
2013-12-05 17:26:21 -0500

    Updated styles to use context layout and added panels responsiveness for 2col

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -6,53 +6,9 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728148
  */
+
+include_once(drupal_get_path('theme', 'ottdoc_zen') . '/templates/header.inc');
 ?>
-<?php if ($logo || $page['top']): ?>
-  <div id="page-top">
-    <div class="inside clearfix">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="top-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      <?php endif; ?>
-
-      <?php print render($page['top']); ?>
-    </div>
-  </div>
-<?php endif; ?>
-<div id="page">
-  <?php if ($site_name || $site_slogan || $secondary_menu || $page['header']): ?>
-    <header class="header" id="header" role="banner">
-      <?php if ($site_name || $site_slogan): ?>
-        <div class="header__name-and-slogan" id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 class="header__site-name" id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div>
-      <?php endif; ?>
-      <?php if ($secondary_menu): ?>
-        <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-          <?php print theme('links__system_secondary_menu', array(
-            'links' => $secondary_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => $secondary_menu_heading,
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-      <?php print render($page['header']); ?>
-    </header>
-  <?php endif; ?>
-
   <div id="main">
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
@@ -111,6 +67,5 @@
       </aside>
     <?php endif; ?>
   </div>
-  <?php print render($page['footer']); ?>
-</div>
-<?php print render($page['bottom']); ?>
+<?php
+include_once(drupal_get_path('theme', 'ottdoc_zen') . '/templates/footer.inc');
