7fa4efe384cfa38c9d31e64b7fe9c5405bf82375
Tiffany Tse <ttse@coldfrontlabs.ca>
2013-12-10 13:49:50 -0500

    Reorganized theme, updated footer template, fixed smacss issues with css

--- templates/region/region--footer.tpl.php
+++ templates/region/region--footer.tpl.php
@@ -8,9 +8,12 @@
  */
 ?>
 <?php if ($content): ?>
-  <footer id="footer" class="<?php print $classes; ?>">
-    <div class="footer-inside">
-      <?php print $content; ?>
-    </div>
-  </footer>
+
+  <footer id="footer" class="<?php print $classes; ?>" role="contentinfo">
+    
+    <?php print $content; ?>
+
+    <p>&copy; 2001-<?php print date("Y");?> <?php print t('City of Ottawa'); ?></p>
+
+  </footer><!-- region__footer -->
 <?php endif; ?>
