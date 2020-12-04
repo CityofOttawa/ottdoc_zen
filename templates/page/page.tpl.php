14f9a78595e9cae37e640e9b4d21579a19ade8a6
Dave Bagler <dave@Baglerit.com>
2020-12-04 13:48:01 -0500

    [BUG] documents.ottawa.ca: navigation and search elements not in a logical tab order (#4)
    * Removing the hidden h1, and making the visible h2 an h1.
    
    * refactoring the page markup to place the navigation markup before the content markup.
    
    * fixing the skip link target.
    
    * add some layout overriding styles to handle our form-as-menu, use case.
    
    * Configuring skip link to go to the search box. Please see https://github.com/CityofOttawa/ottawa_profile/issues/3051\#issuecomment-738542380
    
    * Removing H2 node link focus from the hidden H2s.
    
    * updating the styling to match the display to the tab order, and adjusting a break point to avoid logo/menu overlap.

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -10,6 +10,31 @@
 include_once(drupal_get_path('theme', 'ottdoc_zen') . '/templates/header.inc');
 ?>
   <div id="main">
+      <?php if ($main_menu || $page['navigation']): ?>
+          <div id="navigation">
+              <?php if ($main_menu): ?>
+                  <nav id="main-menu" role="navigation" tabindex="-1">
+                      <?php
+                      // This code snippet is hard to modify. We recommend turning off the
+                      // "Main menu" on your sub-theme's settings form, deleting this PHP
+                      // code block, and, instead, using the "Menu block" module.
+                      // @see https://drupal.org/project/menu_block
+                      print theme('links__system_main_menu', array(
+                          'links' => $main_menu,
+                          'attributes' => array(
+                              'class' => array('links', 'inline', 'clearfix'),
+                          ),
+                          'heading' => array(
+                              'text' => t('Main menu'),
+                              'level' => 'h2',
+                              'class' => array('element-invisible'),
+                          ),
+                      )); ?>
+                  </nav>
+              <?php endif; ?>
+              <?php print render($page['navigation']); ?>
+          </div>
+      <?php endif; ?>
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
@@ -28,32 +53,6 @@ include_once(drupal_get_path('theme', 'ottdoc_zen') . '/templates/header.inc');
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
-    <?php if ($main_menu || $page['navigation']): ?>
-      <div id="navigation">
-        <?php if ($main_menu): ?>
-          <nav id="main-menu" role="navigation" tabindex="-1">
-            <?php
-            // This code snippet is hard to modify. We recommend turning off the
-            // "Main menu" on your sub-theme's settings form, deleting this PHP
-            // code block, and, instead, using the "Menu block" module.
-            // @see https://drupal.org/project/menu_block
-            print theme('links__system_main_menu', array(
-              'links' => $main_menu,
-              'attributes' => array(
-                'class' => array('links', 'inline', 'clearfix'),
-              ),
-              'heading' => array(
-                'text' => t('Main menu'),
-                'level' => 'h2',
-                'class' => array('element-invisible'),
-              ),
-            )); ?>
-          </nav>
-        <?php endif; ?>
-        <?php print render($page['navigation']); ?>
-      </div>
-    <?php endif; ?>
-
     <?php
       // Render the sidebars to see if there's anything in them.
       $sidebar_first  = render($page['sidebar_first']);
